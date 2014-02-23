c2db58bb000e1e99924b91c2e0067f55b4f27e14
James Halliday <mail@substack.net>
2014-02-22 16:09:47 -0800

    configurable outdir

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 var fs = require('fs');
+var path = require('path');
 var minimist = require('minimist');
 var JSONStream = require('JSONStream');
 
-var argv = minimist(process.argv.slice(2));
+var argv = minimist(process.argv.slice(2), {
+    alias: { o: 'outdir' }
+});
+var dir = argv.outdir || process.cwd();
 var by = require('../')();
 
 by.on('region', function (region) {
     console.log('DISTRICT ' + region.name);
-    var outfile = __dirname + '/' + region.name + '.geojson';
+    var outfile = path.join(dir, region.name + '.geojson');
     region.pipe(JSONStream.stringify())
         .pipe(fs.createWriteStream(outfile))
     ;
