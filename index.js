699bf7256fc947732125eecfeddbd3246bc0d65b
James Halliday <mail@substack.net>
2014-02-22 19:57:09 -0800

    save and check unmatched points for new regions

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ inherits(ByPoly, EventEmitter);
 function ByPoly () {
     if (!(this instanceof ByPoly)) return new ByPoly;
     EventEmitter.call(this);
-    this.points = [];
+    this.unmatched = [];
     this.regions = [];
     this.pending = 0;
 }
@@ -38,13 +38,27 @@ ByPoly.prototype.createWriteStream = function () {
             region.stream = points;
             self.regions.push(region);
             self.emit('region', region);
+            
+            for (var i = 0; i < self.unmatched.length; i++) {
+                var u = self.unmatched[i];
+                if (inRegion(u.geometry.coordinates, region.coordinates)) {
+                    region.stream.push(u);
+                    self.unmatched.splice(i, 1);
+                    i--;
+                }
+            }
         }
         else if (geom.type === 'Point') {
-            for (var i = 0; i < self.regions.length; i++) {
+            var len = self.regions.length;
+            for (var i = 0; i < len; i++) {
                 var r = self.regions[i];
                 if (inRegion(geom.coordinates, r.coordinates)) {
                     r.stream.push(feature);
+                    break;
                 }
+            }
+            if (i === len) {
+                self.unmatched.push(feature);
             }
         }
         next();
